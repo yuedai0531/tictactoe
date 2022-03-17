e1e37a5a01cd49cd9034aeecb3be7174ccd65445
Yue Dai <yue.dai@mail.mcgill.ca>
2022-03-17 19:06:34 -0400

    Add functionality for isFinal

--- src/Board.java
+++ src/Board.java
@@ -1,31 +1,56 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Board {
 
     Character[][] state = new Character[3][3];
 
+    public Character isFinal() {
 
-    public Character isFinal(){
+        // check row
+        for (int i = 0; i < this.state.length; i++) {
+            if (Arrays.stream(this.state[i]).allMatch(Objects::nonNull) &&
+                    Arrays.stream(this.state[i]).allMatch(this.state[i][0]::equals)) {
+                return this.state[i][0];
+            }
+        }
 
+        for (int j = 0; j < this.state[0].length; j++) {
+            if(Arrays.stream(this.state).map(x -> x[0]).allMatch(Objects::nonNull) &&
+            Arrays.stream(this.state).map(x -> x[0]).allMatch(this.state[j][0]::equals)){
+                System.out.println(this.state[0][j]);
+                return this.state[0][j];
+            }
+        }
+
+        Arrays.stream(this.state).map(x -> x[2]).forEach(System.out::println);
+//        return this.state[0][j];
+        // check diags
+//        Arrays.stream(this.state).flatMap().allMatch(s -> s[0].equals(this.state[0][0]));
+
+        return 'X';
     }
 
     /**
      * Shows the board state.
      */
-    public void show(){
-        for (int i = 0; i < state.length; i++) {
-            for (int j = 0; j < state[0].length; j++) {
-                System.out.print(state[i][j]==null ? " ":state[i][j]);
-                System.out.print((j!=2)?"|":"");
+    public void show() {
+        for (int i = 0; i < this.state.length; i++) {
+            for (int j = 0; j < this.state[0].length; j++) {
+                System.out.print(this.state[i][j] == null ? " " : this.state[i][j]);
+                System.out.print((j != 2) ? "|" : "");
             }
             System.out.println();
-            System.out.print((i!=2)?"-+-+-":"");
+            System.out.print((i != 2) ? "-+-+-" : "");
             System.out.println();
         }
     }
 
     /**
-    * Resets the board state.
+     * Resets the board state.
      */
-    public void reset(){
+    public void reset() {
         //garbage collect and give new state?
+        this.state = new Character[3][3];
     }
 }
--- src/Play.java
+++ src/Play.java
@@ -5,10 +5,22 @@ public class Play {
         Board board = new Board();
 
         Player p1 = new Player("X", board, 'X');
+        Player p2 = new Player("O", board, 'O');
         board.show();
         board.state[1][1] = 'X';
         board.state[2][2] = 'O';
         p1.addMove(1,2);
+        board.show();
+        board.reset();
+        p1.addMove(0,2);
+        p1.addMove(1,2);
+        p1.addMove(2,2);
+
+//        p2.addMove(0,0);
+//        p2.addMove(0,1);
+//        p2.addMove(0,2);
+
+        board.isFinal();
         board.show();
     }
 }
