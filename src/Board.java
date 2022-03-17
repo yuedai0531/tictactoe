1363eebc86c1b4a0452a477d16a30906dd17ddd1
Yue Dai <yue.dai@mail.mcgill.ca>
2022-03-17 15:56:44 -0400

    Add player

--- src/Board.java
+++ src/Board.java
@@ -1,14 +1,16 @@
 public class Board {
 
-    String[][] state = new String[3][3];
+    Character[][] state = new Character[3][3];
 
-//    public String isFinal(){
-//
-//    }
 
+    public Character isFinal(){
+
+    }
+
+    /**
+     * Shows the board state.
+     */
     public void show(){
-        System.out.println(state);
-
         for (int i = 0; i < state.length; i++) {
             for (int j = 0; j < state[0].length; j++) {
                 System.out.print(state[i][j]==null ? " ":state[i][j]);
@@ -18,5 +20,12 @@ public class Board {
             System.out.print((i!=2)?"-+-+-":"");
             System.out.println();
         }
+    }
+
+    /**
+    * Resets the board state.
+     */
+    public void reset(){
+        //garbage collect and give new state?
     }
 }
--- src/Play.java
+++ src/Play.java
@@ -3,9 +3,12 @@ public class Play {
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe");
         Board board = new Board();
+
+        Player p1 = new Player("X", board, 'X');
         board.show();
-        board.state[1][1] = "X";
-        board.state[2][2] = "O";
+        board.state[1][1] = 'X';
+        board.state[2][2] = 'O';
+        p1.addMove(1,2);
         board.show();
     }
 }
